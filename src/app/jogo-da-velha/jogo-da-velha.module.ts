4c04fbd6b6557da26ffcd0b5bdb75384db5b7d1f
Eduardo Soares <eduardo@dusigner.com.br>
2018-11-19 21:59:14 -0200

    initial project

---
+++ src/app/jogo-da-velha/jogo-da-velha.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { JogoDaVelhaComponent } from './jogo-da-velha.component';
+
+@NgModule({
+  declarations: [JogoDaVelhaComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class JogoDaVelhaModule { }
